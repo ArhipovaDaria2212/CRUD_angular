7b045073b41614c85dd1ef41ebd36e57f2df306c
Arhipova Daria <daria221220042017@gmail.com>
2023-12-25 17:06:22 +0700

    add routing between add and table components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AddPersonComponent } from './components/add-person/add-person.component';
 
 const routes: Routes = [
-  { path: '', component: MainPageComponent }
+  { path: '', component: MainPageComponent },
+  { path: 'add', component: AddPersonComponent }
 ];
 
 @NgModule({
